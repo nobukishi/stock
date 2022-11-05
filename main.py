f351b45a8a5ae584ca2bb0d430ea6c9b07d37f05
岸信利 <kishinobutoshi@kishinobutoshinoMacBook-Air.local>
2022-11-05 22:11:07 +0900

    モーメンタムの判定

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from webbrowser import Konqueror
 from selenium import webdriver
 from time import sleep
 import chromedriver_binary
@@ -5,38 +6,65 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import statistics
+import datetime as dt
+import pandas_datareader.data as web
+import talib as ta
+
 
 options = Options()
 options.add_argument('--headless')
 #driver = webdriver.Chrome('./chromedriver')
 driver = webdriver.Chrome(options=options)
 
+#終値を数日前取得
 def get_owarine(code):
     driver.get(f'https://kabutan.jp/stock/kabuka?code={code}')
     div = driver.find_element(By.ID,'stock_kabuka_table')
     elem_table1= div.find_element(By.CLASS_NAME, "stock_kabuka0")
     elem_table2= div.find_element(By.CLASS_NAME, "stock_kabuka_dwm")
-
+    
     owarine_map={}
 #直近１日目を取得するのにforを使わずに取得し、２日目以降とつなげたい
+    #当日（１日目のみ）
     for elem_tr1 in elem_table1.find_elements(By.XPATH,'tbody/tr'):
         elem_th1 = elem_tr1.find_element(By.XPATH,'th')
         elem_tds1 = elem_tr1.find_elements(By.XPATH,'td')   
-        #print(elem_th.text,elem_tds[3].text)
+        #print(elem_th1.text,elem_tds1[3].text)
         kakaku = elem_tds1[3].text
-        owarine_map[elem_th1.text]=int(kakaku.replace(',',''))   
+        owarine_map[elem_th1.text]=float(kakaku.replace(',',''))   
 
-
+    #２日目以降
     for elem_tr2 in elem_table2.find_elements(By.XPATH,'tbody/tr'):
         elem_th2 = elem_tr2.find_element(By.XPATH,'th')
         elem_tds2 = elem_tr2.find_elements(By.XPATH,'td')   
-        #print(elem_th.text,elem_tds[3].text)
+        #print(elem_th2.text,elem_tds2[3].text)
         kakaku = elem_tds2[3].text
-        owarine_map[elem_th2.text]=int(kakaku.replace(',',''))    
+        owarine_map[elem_th2.text]=float(kakaku.replace(',',''))    
     return owarine_map
 
 
-def check_trend2(owarine_map):
+#当日のmacdを取得
+def get_macdhist(code):
+    ticker_symbol=code
+    ticker_symbol_dr=str(ticker_symbol) + ".T"
+
+    #2022-01-01以降の株価取得
+    start='2022-01-01'
+    end = dt.date.today()
+
+    #データ取得
+    df = web.DataReader(ticker_symbol_dr, data_source='yahoo', start=start,end=end)
+    #print(df)
+    #csv保存
+    #df.to_csv( os.path.dirname(__file__) + '\y_stock_data_'+ ticker_symbol + '.csv')
+    df['macd'], df['macdsignal'], df['macdhist'] = ta.MACD(df['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
+    df[["Open", "High", "Low", "Close", "macd", "macdsignal"]].tail()
+    macdhist = df.iloc[-1,8]
+    return macdhist
+    
+
+#トレンド１が合格しているか
+def check_trend1(owarine_map):
     date_list=list(owarine_map.keys())
     date_list.sort(reverse=True)
     #print(date_list)
@@ -53,8 +81,8 @@ def check_trend2(owarine_map):
         return False 
     return True    
 
-
-def check_trend3(owarine_map):
+#トレンド２が合格しているか
+def check_trend2(owarine_map):
     date_list=list(owarine_map.keys())
     one_kakaku = owarine_map[date_list[0]]
     date_list1 = list(owarine_map.values())
@@ -63,27 +91,36 @@ def check_trend3(owarine_map):
     #print(mean25)
     if one_kakaku < mean25:
         return False
+    return True            
+
+#ＭＡＣＤヒストグラムがプラスか
+def check_macd(macdhist):
+    macd = macdhist
+    if macd < 0:
+        return False
     return True
-    
-    
-    
-    
 
 def main():
-    owarine_map=get_owarine(8609)
-#print(owarine_map)
-    if check_trend2(owarine_map)==False:
-        print('トレンド２に失敗')
+    owarine_map=get_owarine(6632)
+    macd = get_macdhist(6632)
+    if check_trend1(owarine_map)==False:
+        print('トレンド1に失敗')
         return False
-    if check_trend3(owarine_map)==False:
-        print('トレンド3に失敗')
+    if check_trend2(owarine_map)==False:
+        print('トレンド2に失敗')
+        return False
+    if check_macd(macd) ==False:
+        print('macdに失敗')
         return False
     print('合格')
     
+    
 main()
-owarine_map=get_owarine(8609)
+
 #check =check_trend3(owarine_map)
-#print(check)
     
+#print(check_trend3(owarine_map))
+
     
-    
+
+
